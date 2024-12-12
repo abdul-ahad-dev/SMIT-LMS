a05a88cc8ad55fdea0f393ca07e9b2744fb51a72
adilraees786 <adilraees911@gmail.com>
2024-12-12 20:28:30 +0500

    testing

--- src/student/StudentAttendance.jsx
+++ src/student/StudentAttendance.jsx
@@ -1,7 +1,7 @@
 function StudentAttendance() {
   console.log("StudentAttendance");
   return (
-    <div className="bg-green-300">StudentAttendance</div>
+    <div className="bg-green-300 text-red-500">StudentAttendance</div>
   )
 }
 
